39aebd2177c7cd277261b71fce09880394b54fd2
Aeltumn <daniel@goossens.ch>
2025-01-03 18:04:40 +0100

    Fix up MOD_Print

--- Rayman2APMod/src/mod.c
+++ Rayman2APMod/src/mod.c
@@ -55,8 +55,25 @@ LRESULT CALLBACK MOD_WndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 	return GAM_fn_WndProc(hWnd, uMsg, wParam, lParam);
 }
 
-void MOD_Print(const char* text) {
-	fn_vPrint(text);
+// Copied from font.c in R2Console
+long fn_lGetFmtStringLength(char const* szFmt, va_list args) {
+	long lSize = vsnprintf(NULL, 0, szFmt, args);
+	return lSize + 1;
+}
+
+void MOD_Print(char* text, ...) {
+	va_list args;
+	va_start(args, text);
+
+	long lSize = fn_lGetFmtStringLength(text, args);
+	char* szBuffer = _alloca(lSize);
+
+	if (szBuffer) {
+		vsprintf(szBuffer, text, args);
+		fn_vPrint(szBuffer);
+	}
+
+	va_end(args);
 }
 
 void MOD_Main( void )
--- Rayman2APMod/src/mod.h
+++ Rayman2APMod/src/mod.h
@@ -3,5 +3,5 @@
 
 
 LRESULT CALLBACK MOD_WndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
-void MOD_Print(const char*);
+void MOD_Print(char*, ...);
 void MOD_Main(void);

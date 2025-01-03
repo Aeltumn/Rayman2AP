ad68d8e036e3c3a4cc45d11b6c87396a41979ba6
Aeltumn <daniel@goossens.ch>
2025-01-03 21:05:02 +0100

    Use vcpkg to import APCpp

--- Rayman2APMod/src/mod.h
+++ Rayman2APMod/src/mod.h
@@ -1,10 +1,6 @@
 #pragma once
 #include "framework.h"
 
-
-LRESULT CALLBACK MOD_WndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
-
-
 void MOD_Print(char*, ...);
 void MOD_Main(void);
 
